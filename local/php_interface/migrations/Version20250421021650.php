c3dcdd443c6fe3dcffa744320b999d9be8e82078
Robert-Ace <beeeshop@yandex.ru>
2025-04-21 02:18:06 +0300

    Create migration HL block

---
+++ local/php_interface/migrations/Version20250421021650.php
@@ -0,0 +1,491 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20250421021650 extends Version
+{
+    protected $author = "admin";
+
+    protected $description = "";
+
+    protected $moduleVersion = "5.0.0";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+    $hlblockId = $helper->Hlblock()->saveHlblock(array (
+  'NAME' => 'IpInfo',
+  'TABLE_NAME' => 'ip_info',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'ip-адреса',
+    ),
+    'en' => 
+    array (
+      'NAME' => 'ip-adreses',
+    ),
+  ),
+));
+        $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_IP_ADDRESS',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => 'UF_IP_ADDRESS',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 11,
+    'MAX_LENGTH' => 14,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'IP address',
+    'ru' => 'IP адрес',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_CITY_ID',
+  'USER_TYPE_ID' => 'integer',
+  'XML_ID' => 'UF_CITY_ID',
+  'SORT' => '101',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'MIN_VALUE' => 0,
+    'MAX_VALUE' => 0,
+    'DEFAULT_VALUE' => NULL,
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'City ID',
+    'ru' => 'ID города',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_CITY_NAME_RU',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => 'UF_CITY_NAME_RU',
+  'SORT' => '102',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'City name ru',
+    'ru' => 'Название города на русском',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_CITY_NAME_EN',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => 'UF_CITY_NAME_EN',
+  'SORT' => '104',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'City name en',
+    'ru' => 'Название города на английском',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_REGION_ID',
+  'USER_TYPE_ID' => 'integer',
+  'XML_ID' => 'UF_REGION_ID',
+  'SORT' => '105',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'MIN_VALUE' => 0,
+    'MAX_VALUE' => 0,
+    'DEFAULT_VALUE' => NULL,
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Region ID',
+    'ru' => 'ID региона',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_REGION_NAME_RU',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => 'UF_REGION_NAME_RU',
+  'SORT' => '106',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Region name ru',
+    'ru' => 'Название региона на русском',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_REGION_NAME_EN',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => 'UF_REGION_NAME_EN',
+  'SORT' => '107',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Region name en',
+    'ru' => 'Название региона на английском',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_COUNTRY_ID',
+  'USER_TYPE_ID' => 'integer',
+  'XML_ID' => 'UF_COUNTRY_ID',
+  'SORT' => '108',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'MIN_VALUE' => 0,
+    'MAX_VALUE' => 0,
+    'DEFAULT_VALUE' => NULL,
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Country ID',
+    'ru' => 'ID страны',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_COUNTRY_NAME_RU',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => 'UF_COUNTRY_NAME_RU',
+  'SORT' => '109',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Country name ru',
+    'ru' => 'Название страны на русском',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_COUNTRY_NAME_EN',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => 'UF_COUNTRY_NAME_EN',
+  'SORT' => '110',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Country name en',
+    'ru' => 'Название страны на английском',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+        }
+}
